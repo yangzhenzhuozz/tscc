696d70f384a0dd26f694a4bfd5f55df3c390f3cc
yangzhenzhuozz <yangzhenzhuozz@126.com>
2021-08-12 17:16:55 +0800

    Update toy-language.ts

--- src/example/toy-language/toy-language.ts
+++ src/example/toy-language/toy-language.ts
@@ -1,88 +1,133 @@
-import  fs  from "fs";
+import fs from "fs";
 import TSCC from "../../tscc/tscc.js";
 import { Grammar } from "../../tscc/tscc.js";
 let grammar: Grammar = {
-    tokens: ["+", ",", "-", "*", "/", "=", "==", "||", "&&", ">", "<", ">=", "<=", "{", "}", "(", ")", ":", ";", ".", "func", "id", "var", "val", "type", "class", "struct", "if", "else", "do", "while", "for", "const_val"],
+    tokens: ['val', 'var', 'id', 'type_base', ':', ';', '.', ',', 'class', 'function', '!', '{', '}', '(', ')', 'if', 'else', 'while', 'do', 'for', '=', '[', ']', 'new', 'const_val', 'break', 'continue', 'get', 'set', 'return', 'extend', 'switch', 'case', 'default', 'operator'],
     association: [
+        { "nonassoc": ['low_priority'] },//低优先级的一个临时符号
+        { "right": ['else'] },
         { "right": ["="] },
         { "left": ["=="] },
         { "left": ["||"] },
         { "left": ["&&"] },
-        { "nonassoc": [">", "<", ">=", "<="] },//不允许这几个符号的连续出现
+        { "nonassoc": [">", "<", ">=", "<="] },
         { "left": ["+", "-"] },
         { "left": ["*", "/"] },
-        { "nonassoc": ["uminus"] }//一元减法,取反
+        { "nonassoc": ["uminus"] },//一元减法,取反
+        { "left": ["!"] },
+        { "right": ['['] },//取下标
+        { "right": ['.'] },//.运算符有很高的优先级,a+c.d可以解释成 a + (c.d)或者(a+c).d 出现二义性，所以需要定义优先级
     ],
     BNF: [
         { "program:units": {} },
         { "units:units unit": {} },
-        { "units:": {} },
-        { "unit:function": {} },
-        { "unit:_class": {} },
-        { "function:func id ( args ) type_declare block": {} },//带返回值声明的函数定义
-        { "args:arg_list": {} },//带参数
-        { "args:": {} },//不带参数
-        { "arg_list:arg_list , argument": {} },
-        { "arg_list:argument": {} },
-        { "argument: object": {} },
-        { "block:{ code_units }": {} },
-        { "code_units:code_units code_unit": {} },
-        { "code_units:": {} },//代码单元
-        { "code_unit:declare": {} },
-        { "code_unit:statement": {} },
-        { "declare:value": {} },
-        { "declare:variable": {} },
-        //val类型必须带initiator
-        { "value:val id type_declare initiator ;": {} },
-        { "value:val id initiator ;": {} },
-        //var类型就可以略微自由一点
-        { "variable:var id type_declare initiator ;": {} },
-        { "variable:var id type_declare ;": {} },
-        { "variable:var id initiator ;": {} },
-        { "initiator:= object": {} },
-        { "type_declare: : type ": {} },//普通类型
-        { "type_declare: : func ( parameter_types ) ": {} },//函数类型
-        { "parameter_types:parameter_type_list": {} },
-        { "parameter_types:": {} },
-        { "parameter_type_list:parameter_type_list , type": {} },//函数声明
-        { "parameter_type_list:type": {} },
-        { "_class:class id { class_units }": {} },
-        { "_class:struct id { class_units }": {} },
-        { "class_units:class_units class_unit": {} },
-        { "class_units:": {} },
-        { "class_unit:declare": {} },
-        { "class_unit:function": {} },
-        { "statement: object ;": {} },//单独一个对象也是一条语句
-        { "statement: if ( object ) statement ;": {} },
-        { "statement: if ( object ) statement else statement ;": {} },
-        { "statement: while ( object ) statement ;": {} },
-        { "statement: do statement while ( object ) ;": {} },
-        { "statement: for ( object ; object ; object )": {} },
-        { "statement: for ( declare ; object ; object )": {} },
-        { "object: object + object": {} },
-        { "object: object - object": {} },
-        { "object: object * object": {} },
-        { "object: object / object": {} },
-        { "object: object == object": {} },
-        { "object: object || object": {} },
-        { "object: object && object": {} },
-        { "object: object > object": {} },
-        { "object: object < object": {} },
-        { "object: object >= object": {} },
-        { "object: object <= object": {} },
-        { "object: ( object )": {} },
-        { "object: - object": { priority: "uminus" } },//一元减法
-        { "object: object = object": {} },//允许连续赋值
-        { "object: const_val": {} },
-        { "object: id": {} }
+        { "units:": {} },//最外层的代码单元分别为声明和代码
+        { "unit:declare ;": {} },//变量声明
+        { "unit:class_def": {} },//class
+        { "unit:function_def": {} },//function
+        { "unit:operator_overload": {} },//操作符重载
+        { "unit:statement": {} },//语句
+        { "declare:val id : type = object ": {} },//declare为var和val
+        { "declare:val id = object ": {} },
+        { "declare:var id : type = object": {} },//var类型相比val多了一个不赋值的定义
+        { "declare:var id : type": {} },
+        { "declare:var id = object": {} },
+        { "declare:val id : type { getter }": {} },
+        { "declare:var id : type { getter setter }": {} },
+        { "getter:get block": {} },
+        { "setter:set block": {} },
+        { "class_def: class id extend_or_empty { units }": {} },
+        { "class_def: class val id extend_or_empty { units }": {} },//值类型的class
+        { "extend_or_empty:": {} },
+        { "extend_or_empty:extend : base_types": {} },
+        { "base_types:base_types , id": {} },
+        { "base_types:id": {} },
+        { "function_def:function id ( parameters_or_empty ) : type block": {} },//语法糖,与函数对象定义功能一致
+        { "parameters_or_empty:": {} },
+        { "parameters_or_empty:parameters": {} },
+        { "parameters:parameters , id : type": {} },
+        { "parameters:id : type": {} },
+        { "block:{ units }": {} },
+        { "statement:object ;": {} },
+        { "statement:if ( object ) statement": { priority: "low_priority" } },//让if语句优先级低于if-else
+        { "statement:if ( object ) statement else statement": {} },
+        { "statement:loop_flag while ( object ) statement": {} },
+        { "statement:loop_flag do statement while ( object ) ;": {} },
+        { "statement:loop_flag for ( for_initiate ; for_condition ; for_step  ) statement": {} },
+        { "loop_flag:": {} },
+        { "loop_flag:id :": {} },
+        { "statement:break ;": {} },
+        { "statement:break id ;": {} },
+        { "statement:continue ;": {} },
+        { "statement:continue id ;": {} },
+        { "statement:return ;": {} },
+        { "statement:return object ;": {} },
+        { "statement:block": {} },
+        { "statement:switch_case": {} },
+        { "for_initiate:": {} },
+        { "for_initiate:declare": {} },
+        { "for_initiate:assignment": {} },
+        { "for_condition:": {} },
+        { "for_condition:object": {} },
+        { "for_step:": {} },
+        { "for_step:object": {} },
+        { "switch_case:switch ( object ) { switch_bodys_or_empty }": {} },
+        { "switch_bodys_or_empty:": {} },
+        { "switch_bodys_or_empty:switch_bodys": {} },
+        { "switch_bodys:switch_bodys switch_body": {} },
+        { "switch_bodys:switch_body": {} },
+        { "switch_body:case object : statement": {} },
+        { "switch_body:default : statement": {} },
+        { "assignment:object = object": {} },//赋值语句
+        { "object:id": {} },//一切皆对象,没有表达式这个概念了,所有操作得到的都是一个对象
+        { "object:object . id": {} },//取成员
+        { "object:object . id ( arguments_or_empty )": {} },//调用成员函数
+        { "object:const_val": {} },
+        { "object:function ( parameters_or_empty ) : type block": {} },//定义一个函数对象
+        { "object:new type ( )": {} },//调用构造函数
+        { "object:type ( )": {} },//值类型不需要用new就可以调用构造函数
+        { "object:id ( arguments_or_empty )": {} },//函数调用
+        { "object:( object )": {} },
+        { "object:assignment": {} },
+        { "object:object + object": {} },
+        { "object:object - object": {} },
+        { "object:object * object": {} },
+        { "object:object / object": {} },
+        { "object:object < object": {} },
+        { "object:object <= object": {} },
+        { "object:object > object": {} },
+        { "object:object >= object": {} },
+        { "object:object || object": {} },
+        { "object:object && object": {} },
+        { "object:! object": {} },
+        { "object:object [ id ]": {} },
+        { "arguments_or_empty:": {} },
+        { "arguments_or_empty:arguments": {} },
+        { "arguments:arguments , object": {} },
+        { "arguments:object": {} },
+        { "type:type_base array_def": {} },//数组类型
+        { "array_def:[ ] array_def": {} },
+        { "array_def:": {} },
+        { "type:function ( parameters_or_empty ) : type": {} },//函数类型
+        { "operator_overload:operator + ( id : type ) : type block": {} },//操作符重载+
+        { "operator_overload:operator - ( id : type ) : type block": {} },//操作符重载-
+        { "operator_overload:operator * ( id : type ) : type block": {} },//操作符重载*
+        { "operator_overload:operator / ( id : type ) : type block": {} },//操作符重载/
+        { "operator_overload:operator < ( id : type ) : type block": {} },//操作符重载<
+        { "operator_overload:operator > ( id : type ) : type block": {} },//操作符重载>
+        { "operator_overload:operator <= ( id : type ) : type block": {} },//操作符重载<=
+        { "operator_overload:operator >= ( id : type ) : type block": {} },//操作符重载>=
+        { "operator_overload:operator || ( id : type ) : type block": {} },//操作符重载||
+        { "operator_overload:operator && ( id : type ) : type block": {} },//操作符重载&&
+        { "operator_overload:operator ! ( ) : type block": {} },//操作符重载!
+        { "operator_overload:operator [ ] ( id : type ) : type block": {} },//操作符重载[]
     ]
 };
-
 let tscc = new TSCC(grammar, { language: "zh-cn", debug: false });
 let str = tscc.generate();//构造编译器代码
 if (str != null) {//如果构造成功,往代码中添加调用代码
     console.log(`成功`);
-    fs.writeFileSync('./src/example/toy-language/parser.ts',str);
+    fs.writeFileSync('./src/example/toy-language/parser.ts', str);
 } else {
     console.log(`失败`);
 }
