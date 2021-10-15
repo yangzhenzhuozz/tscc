5919e61608b0aaefa229e156a7ca6c54d67297ca
yangzhenzhuozz <yangzhenzhuozz@126.com>
2021-10-15 18:00:30 +0800

    Update toy-language.ts

--- src/example/toy-language/toy-language.ts
+++ src/example/toy-language/toy-language.ts
@@ -1,50 +1,161 @@
 import fs from "fs";
 import TSCC from "../../tscc/tscc.js";
 import { Grammar } from "../../tscc/tscc.js";
-import { scope } from './scope.js'
+import { Scope, Quadruple, TmpScope, Address } from './lib.js'
 
 let grammar: Grammar = {
-    userCode: `import {scope} from './scope.js'`,//让自动生成的代码包含import语句
-    tokens: ['var', ';', 'id', 'number', '+'],
+    userCode: `import { Scope, Quadruple, TmpScope, Address } from './lib.js'`,//让自动生成的代码包含import语句
+    tokens: ['var', ';', 'id', 'number', '+', '(', ')'],
     association: [
-        { 'left': ['+'] },
-        { 'right': ['='] }
+        { 'right': ['='] },
+        { 'left': ['==', '!='] },
+        { 'left': ['||'] },
+        { 'left': ['&&'] },
+        { 'left': ['!'] },
+        { 'left': ['>', '<', '<=', '>='] },
+        { 'left': ['+', '-'] },
+        { 'left': ['*', '/'] },
     ],
     BNF: [
         //当一个继承属性需要往下传递的时候，创建一个临时非终结符保存这个属性
         {
-            "program:W0 units": {//program.scope=new scope();units.scope=program.scope
-                action:function(args,stack){
-                    console.table(args[0].scope.symTable);
-                }
-            }
-        },
-        {
-            "W0:": {
-                action: function () {
-                    return { scope: new scope() };
+            "program:program_W0 units": {//program.scope=new scope();units.scope=program.scope
+                action: function (args, stack) {
                 }
             }
         },
         { "units:unit W2 units": {} },//unit.scope=units.scope;units1.scope=units.scope
-        {
-            "W2:": {
-                action: function (args, stack) {
-                    let sym = stack.slice(-2);
-                    return { scope: sym[0].scope };
-                }
-            }
-        },
         { "units:": {} },
         { "unit:declare": {} },//declare.scope=unit.scope
         {
             "declare:var id ;": {//declare.scope.addSym(id.name)
                 action: function (args, stack) {
-                    let sym = stack.slice(-1);
-                    sym[0].scope.addSym(args[1].name);
+                    let head = stack.slice(-1)[0];
+                    head.scope.addSym(args[1].name);
                 }
             }
         },
+        {
+            "unit:stmt_W1 stmt": {//stmt.scope=unit.scope,stmt.tmpScope=new scope
+                action: function (args, stack) {
+                }
+            }
+        },
+        {
+            "stmt:object ;": {//object.scope=stmt.scope
+                action: function (args, stack) {
+                    if (args[0].code != undefined) {
+                        for (let p of args[0].code) {
+                            console.log(`${p}`);
+                        }
+                    }
+                }
+            }
+        },
+        {
+            "object:id": {
+                action: function (args, stack) {
+                    let head = stack.slice(-1)[0];
+                    let name = args[0].name;
+                    let address = head.scope.getSym(name);
+                    return { type: `number`, address: new Address('varible', address) };
+                }
+            }
+        },
+        {
+            "object:number": {
+                action: function (args, stack) {
+                    return { type: `number`, address: new Address('constant', args[0]) };
+                }
+            }
+        },
+        {
+            "object:( W2 object )": {
+                action: function (args, stack) {
+                    return args[2];
+                }
+            }
+        },
+        {
+            "object:object + W3 object": {
+                action: function (args, stack) {
+                    let head = stack.slice(-1)[0];
+                    let resultAddress = new Address('temporary', head.tmpScope.addSym());
+                    let A = args[0];
+                    let B = args[3];
+                    let code = new Array<Quadruple>();
+                    if (A.code != undefined) {
+                        code = code.concat(A.code);
+                    }
+                    if (B.code != undefined) {
+                        code = code.concat(B.code);
+                    }
+                    code.push(new Quadruple('+', A.address, B.address, resultAddress));
+                    return { type: 'number', address: resultAddress, code: code }
+                }
+            }
+        },
+        { "object:object - W3 object": {} },
+        { "object:object * W3 object": {} },
+        { "object:object / W3 object": {} },
+        { "object:object > W3 object": {} },
+        { "object:object == W3 object": {} },
+        { "object:object != W3 object": {} },
+        { "object:object >= W3 object": {} },
+        { "object:object < W3 object": {} },
+        { "object:object <= W3 object": {} },
+        { "object:object && W3 object": {} },
+        { "object:object || W3 object": {} },
+        { "object:! W2 object": {} },
+        {
+            "object:object = W3 object": {
+                action: function (args, stack) {
+                    let result = args[0];
+                    let A = args[3];
+                    if(result.address.location!='varible'){
+                        throw `赋值语句的左侧必须是左值`;
+                    }
+                    let code = new Array<Quadruple>();
+                    if (A.code != undefined) {
+                        code = code.concat(A.code);
+                    }
+                    code.push(new Quadruple('=', A.address, null, result.address));
+                    return { type: 'number', address: result.address, code: code };
+                }
+            }
+        },
+        {
+            "program_W0:": {
+                action: function () {
+                    return { scope: new Scope() };
+                }
+            }
+        },
+        {
+            "stmt_W1:": {
+                action: function (args, stack) {
+                    let head = stack.slice(-1)[0];
+                    head.tmpScope = new TmpScope();
+                    return head;
+                }
+            }
+        },
+        {
+            "W2:": {
+                action: function (args, stack) {
+                    let head = stack.slice(-2)[0];
+                    return head;
+                }
+            }
+        },
+        {
+            "W3:": {
+                action: function (args, stack) {
+                    let head = stack.slice(-3)[0];
+                    return head;
+                }
+            }
+        }
     ]
 };
 let tscc = new TSCC(grammar, { language: "zh-cn", debug: false });
