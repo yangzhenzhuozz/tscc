2d9085518e8a369d304183223183000ec7b95dd4
john <775697360@qq.com>
2022-02-15 22:53:19 +0800

    重新设计语法

--- src/example/toy-language-3/test.ts
+++ src/example/toy-language-3/test.ts
@@ -1,9 +1,17 @@
-import { Type, Address } from "./lib.js";
-let int=new Type("int","referentialType");
-let A=new Type("A","valuetype");
-let B=new Type("B","valuetype");
-A.registerField("b",new Address("class",B,0));
-B.registerField("a",new Address("class",A,0));
-A.setParent(B);
-B.setParent(A);
-B.checkRecursive();
+// import { Type, Address } from "./lib.js";
+// let int=new Type("int","referentialType");
+// let A=new Type("A","valuetype");
+// let B=new Type("B","valuetype");
+// A.registerField("b",new Address("class",B,0));
+// B.registerField("a",new Address("class",A,0));
+// A.setParent(B);
+// B.setParent(A);
+// B.checkRecursive();
+console.log("a");
+class A {
+    public a:number=this.f();
+    public f(): number {
+        return this.a;
+    }
+}
+console.log(new A())
